94bcfbc9a8533581833c2f27b4bad77054b2c414
caimi124 <chupingzeng@gmail.com>
2026-01-04 22:17:46 +0800

    fix: clean point page entry to use shared template

--- app/knowledge/point/[id]/page.tsx
+++ app/knowledge/point/[id]/page.tsx
@@ -1,3 +1,13 @@
+'use client'
+
+import { useParams } from 'next/navigation'
+import { PointDetailPage } from '@/components/knowledge/PointDetailPage'
+
+export default function KnowledgePointPage() {
+  const params = useParams()
+  const pointId = params.id as string
+  return <PointDetailPage pointId={pointId} />
+}
 import { useParams } from 'next/navigation'
 import { PointDetailPage } from '@/components/knowledge/PointDetailPage'
 
